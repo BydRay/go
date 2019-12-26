8f2aa38065af4d25f7de7660c8fe5999b34de7d9
Byd Ray <775958771@qq.com>
2019-12-26 12:35:55 +0800

    updata ignore

---
+++ project/src/dev/day1/goroute/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 100; i++ {
+		go test_goroute(i)
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println("end")
+}
---
+++ project/src/dev/day2/example2/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	a "dev/day2/example2/add"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Name = ", a.Name)
+	fmt.Println("age = ", a.Age)
+}
