38dbe2df0ec6b8664480770371f92cfa0e827f91
Byd Ray <775958771@qq.com>
2019-12-29 23:24:07 +0800

    20191229_day4ing

--- project/src/dev/day4/example/example1/main.go
+++ project/src/dev/day4/example/example1/main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
 	var a []int
 	a = append(a, 2)
 	a = append(a, a...)
+
+	var b int
+	fmt.Printf("%T\n", b)
+
+	var arr [5]int
+	arr2 := arr
+	arr2[2] = 100 // arr1 不会改变
+
+	fmt.Println(arr)
+	fmt.Println(arr2)
+
+	str := "asdf"
+	str[2] = 'a'
+	fmt.Println(str)
 }
